06e54c191d3da1aaa5bd024104034f73aff1f445
Hannah Zuber <hgomberg@gmail.com>
2019-11-08 08:19:44 +0100

    setting up

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
-const app = require('./app');
+require('dotenv').config();
+const { PORT } = process.env.PORT || 8000;
+const express = require('express');
+const app = express();
 
-const {PORT} = require('./config')
+app.get('/api/*', (req, res) => {
+    res.json({ ok: true })
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost: ${PORT}`)
 })
+
+module.exports = {app}
