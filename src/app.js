9b3e04163d85034f07bcc41a4b75e08ef0d3bb37
Hannah Zuber <hgomberg@gmail.com>
2019-11-25 10:23:37 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -14,25 +14,15 @@ const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common');
 
 app.use(morgan(morganOption));
 app.use(helmet());
-var whitelist = [CLIENT_ORIGIN, 'localhost:3000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
- 
-app.get('/', cors(corsOptionsDelegate), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
-})
+app.use(cors());
 
 app.use('/api/share-my-stuff', shareRouter)
 app.use('/api/users', UsersRouter)
 app.use('/api/items', ItemsRouter)
 
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
 
 /*app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
