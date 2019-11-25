6da115a696a95cd8dcab182d74fd4dbf0d84064e
Hannah Zuber <hgomberg@gmail.com>
2019-11-25 09:44:20 +0100

    MVP

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/share_my_stuff",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/share_my_stuff-test",
-    CLIENT_ORIGIN: "http://localhost:3000" || 'https://share-my-stuff.hzuber.now.sh/'
+    CLIENT_ORIGIN: 'https://share-my-stuff.hzuber.now.sh/'
 }
